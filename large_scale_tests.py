db8945c31ee60ea4a694a4b0adb4150203e24a23
Hafiz <issahhafiz@gmail.com>
2024-06-12 16:23:55 -0600

    reviewing large scale tests

--- large_scale_tests.py
+++ large_scale_tests.py
@@ -1,7 +1,20 @@
-# This python file is to test coherence analyses for a larger dataset. 
-# The data is from Brady Hotspring and is in hdf5 format.
+"""This python file is to test coherence analyses for a larger dataset. The was written for some 
+data from Brady Geothermal DAS experiment and is in hdf5 format. Can be ran as:
+python large_scale_test.py <data_location> <averaging_window_length> <sub_window_length> <overlap> <first_channel> <num_channels> <samples_per_sec> <channel_offset> <method> <batch> <batch_size>
+- data_location: path to the directory containing the data files
+- averaging_window_length: Averaging window length in seconds
+- sub_window_length: sub-window length in seconds
+- overlap: overlap in seconds
+- first_channel: first channel
+- num_channels: number of sensors
+- samples_per_sec: samples per second
+- channel_offset: channel offset
+- method: method to use for coherence analysis
+- batch: Batch of files assuming jobs are run in parallel for files in batches. Should be one if that is not the case.
+- batch_size: Number of files in batch. Should be number of files being considered if job is not done in batches.
+The script will then go through the files in the batch and perform coherence analysis on the data"""
 # This is not complete and is a work in progress.
-# python coherence_test.py <file> <averaging_window_length> <sub_window_length> <overlap> <first_channel> <num_channels> <samples_per_sec> <channel_offset> <method> <batch> <batch_size>
+
 
 import os
 import pickle
@@ -19,7 +32,7 @@ if __name__ == "__main__":
     METHODS = ['exact', 'qr', 'svd', 'rsvd', 'power', 'qr iteration']
 
     # Take inputs from the command line
-    file = sys.argv[1]
+    data_basepath = sys.argv[1] # Path to the directory containing the data files
     averaging_window_length = int(sys.argv[2]) # Averaging window length in seconds
     sub_window_length = int(sys.argv[3]) # sub-window length in seconds
     overlap = int(sys.argv[4]) # overlap in seconds
@@ -32,11 +45,10 @@ if __name__ == "__main__":
     batch_size = int(sys.argv[11]) # Number of files in batch. Should be number of files being considered if job is not done in batches.
 
     # Path to the directory containing the data files
-    data_basepath = "/beegfs/projects/martin/BradyHotspring"  # "D:/CSM/Mines_Research/Test_data/Brady Hotspring"
+    # data_basepath = "/beegfs/projects/martin/BradyHotspring"  # "D:/CSM/Mines_Research/Test_data/Brady Hotspring"
 
     # Path to the directory where the results will be saved
     save_location = "/u/st/by/aissah/scratch/event_detection/template_matching"  # "D:/CSM/Mines_Research/Test_data/"
-    # samples_per_sec = 1000
 
     # Get the file names of the data files by going through the folders contained
     # in the base path and putting together the paths to files ending in .h5
@@ -54,6 +66,8 @@ if __name__ == "__main__":
 
     # create a dictionary to store the metadata of the files
     metadata = {}
+
+    # load the first file in the batch
     if batch == 1:
         first_file_time = data_files[0][-15:-3]
 
@@ -70,21 +84,23 @@ if __name__ == "__main__":
             metadata["files"] = [a[-15:-3] for a in data_files]
         data, _ = func.loadBradyHShdf5(data_files[1], normalize="no")
         data = data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)]
-        # preceding_data, _ = func.loadBradyHShdf5(data_files[0], normalize="no")
-        # data = np.append(
-        #     preceding_data[first_channel:last_channel, -len(template[1]) + 1 :],
-        #     data[first_channel:last_channel],
-        #     axis=1,
-        # )
+
+    # check if the data is long enough to be used for the analysis
+    # if not, load the next file and append to the data
+    len_data = data.shape[1]/samples_per_sec
+    while len_data < averaging_window_length:
+        next_data, _ = func.loadBradyHShdf5(data_files[0], normalize="no")
+        next_data = next_data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)]
+        data = np.append(data, next_data[:, :averaging_window_length*samples_per_sec], axis=1)
+        len_data = data.shape[1]/samples_per_sec
 
     # work on files after first file in batch. This works exactly as we handled the
     # beginning of later batches. Then we keep appending to the variables set up for
     # first file of the batch above
-
     if method == 'qr':
-        detection_significances, eig_estimatess = func.coherence(data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)], sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
+        detection_significances, eig_estimatess = func.coherence(data, sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
     elif method in METHODS:
-        detection_significances = func.coherence(data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)], sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
+        detection_significances = func.coherence(data, sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
 
     # norm_win_spectra, frequencies = func.normalised_windowed_spectra(data[first_channel:num_channels+first_channel:int(num_channels/nsensors)], sub_window_length, overlap, sample_interval=1/samples_per_sec)
     # welch_coherence_mat = np.matmul(norm_win_spectra, np.conjugate(norm_win_spectra.transpose(0,2,1)))
@@ -106,14 +122,16 @@ if __name__ == "__main__":
     print(f"First file completed in: {end_time - start_time}", flush=True)
 
     for a in data_files[1:]:
-        # preceding_data = data[:, -len(template[1]) + 1 :]
+        preceding_data = data[:, -overlap*samples_per_sec:]
         data, _ = func.loadBradyHShdf5(a, normalize="no")
         # data = np.append(preceding_data, data[first_channel:last_channel], axis=1)
         data = data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)]
         if method == 'qr':
-            detection_significance, eig_estimates = func.coherence(data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)], sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
+            detection_significance, eig_estimates = func.coherence(data, sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
         elif method in METHODS:
-            detection_significance = func.coherence(data[first_channel:channel_offset+first_channel:int(channel_offset/num_channels)], sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
+            detection_significance = func.coherence(data, sub_window_length, overlap, sample_interval=1/samples_per_sec, method=method)
+        else:
+            raise ValueError(f"Method {method} not available for coherence analysis")
 
         if detection_significance.shape == detection_significances.shape:
             detection_significances = np.append(detection_significances[np.newaxis], detection_significance[np.newaxis], axis=0)
