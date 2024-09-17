963c0d5f0b1252380badfd7f518503ac9e3dc49f
Hafiz <issahhafiz@gmail.com>
2024-09-17 13:04:58 -0600

    work dasdae in =to large-scale tests

--- large_scale_tests copy.py
+++ large_scale_tests copy.py
@@ -32,92 +32,93 @@ import pickle
 import sys
 from datetime import datetime
 
+import dascore as dc
 import numpy as np
 
 import functions as func
 
 
-def _next_data_window(
-    data_files: list[str],
-    next_index: int,
-    averaging_window_length: int,
-    samples_per_sec: int,
-    start_sample_index: int = 0,
-):
-    """
-    Load the next data window from the data files. This function is used to
-    load the next window of data from the list of data files. It continues
-    to read data from the files until the window length is reached. The
-    function returns the data, the index of the next file to read data from,
-    and the index with the file at which we stopped reading.
+# def _next_data_window(
+#     data_files: list[str],
+#     next_index: int,
+#     averaging_window_length: int,
+#     samples_per_sec: int,
+#     start_sample_index: int = 0,
+# ):
+#     """
+#     Load the next data window from the data files. This function is used to
+#     load the next window of data from the list of data files. It continues
+#     to read data from the files until the window length is reached. The
+#     function returns the data, the index of the next file to read data from,
+#     and the index with the file at which we stopped reading.
 
-    Parameters
-    ----------
-    data_file : list[str]
-        list of the data files to read data from
-    next_index : int
-        index of the next file to read data from
-    averaging_window_length : int
-        length of the averaging window in seconds
-    samples_per_sec : int
-        number of samples per second in the data
-    start_sample_index : int
-        index of the first sample to read from the next data file
+#     Parameters
+#     ----------
+#     data_file : list[str]
+#         list of the data files to read data from
+#     next_index : int
+#         index of the next file to read data from
+#     averaging_window_length : int
+#         length of the averaging window in seconds
+#     samples_per_sec : int
+#         number of samples per second in the data
+#     start_sample_index : int
+#         index of the first sample to read from the next data file
 
-    Returns
-    -------
-    data : np array
-        data read from the data files
-    next_index : int
-        index of the next file to read data from
-    stop_sample_index : int
-        index we stopped reading data from file "next_index"
+#     Returns
+#     -------
+#     data : np array
+#         data read from the data files
+#     next_index : int
+#         index of the next file to read data from
+#     stop_sample_index : int
+#         index we stopped reading data from file "next_index"
 
-    """
+#     """
 
-    num_files = len(data_files)
-    total_window_length = averaging_window_length * samples_per_sec
-    data, _ = func.loadBradyHShdf5(data_files[next_index], normalize="no")
-    data_len = data.shape[1]
-    data = data[
-        first_channel : channel_offset
-        + first_channel : int(channel_offset / num_channels),
-        start_sample_index : start_sample_index + total_window_length,
-    ]
+#     num_files = len(data_files)
+#     total_window_length = averaging_window_length * samples_per_sec
+#     data, _ = func.loadBradyHShdf5(data_files[next_index], normalize="no")
+#     data_len = data.shape[1]
+#     data = data[
+#         first_channel : channel_offset
+#         + first_channel : int(channel_offset / num_channels),
+#         start_sample_index : start_sample_index + total_window_length,
+#     ]
 
-    stop_sample_index = (
-        start_sample_index + total_window_length
-    )  # index we stopped reading data from file "next_index"
+#     stop_sample_index = (
+#         start_sample_index + total_window_length
+#     )  # index we stopped reading data from file "next_index"
 
-    # number of samples to add to the data to make up the window length
-    window_deficit = total_window_length - data.shape[1]
+#     # number of samples to add to the data to make up the window length
+#     window_deficit = total_window_length - data.shape[1]
 
-    if window_deficit == 0 and stop_sample_index == data_len:
-        next_index += 1
-        stop_sample_index = 0
+#     if window_deficit == 0 and stop_sample_index == data_len:
+#         next_index += 1
+#         stop_sample_index = 0
 
-    while window_deficit > 0 and next_index < num_files - 1:
-        next_index += 1  # index of the next file to read data from
-        next_data, _ = func.loadBradyHShdf5(
-            data_files[next_index],
-            normalize="no",
-        )
-        next_data = func.rm_laser_drift(next_data)
-        next_data = next_data[
-            first_channel : channel_offset
-            + first_channel : int(channel_offset / num_channels)
-        ]
-        data = np.append(data, next_data[:, :window_deficit], axis=1)
+#     while window_deficit > 0 and next_index < num_files - 1:
+#         next_index += 1  # index of the next file to read data from
+#         next_data, _ = func.loadBradyHShdf5(
+#             data_files[next_index],
+#             normalize="no",
+#         )
+#         next_data = func.rm_laser_drift(next_data)
+#         next_data = next_data[
+#             first_channel : channel_offset
+#             + first_channel : int(channel_offset / num_channels)
+#         ]
+#         data = np.append(data, next_data[:, :window_deficit], axis=1)
 
-        if window_deficit < next_data.shape[1]:
-            stop_sample_index = window_deficit
-        elif window_deficit == next_data.shape[1] or next_index == num_files - 1:
-            next_index += 1
-            stop_sample_index = 0
+#         if window_deficit < next_data.shape[1]:
+#             stop_sample_index = window_deficit
+#         elif window_deficit == next_data.shape[1] or next_index == num_files - 1:
+#             next_index += 1
+#             stop_sample_index = 0
 
-        window_deficit = total_window_length - data.shape[1]
+#         window_deficit = total_window_length - data.shape[1]
 
-    return data, next_index, stop_sample_index
+#     return data, next_index, stop_sample_index
 
 
 if __name__ == "__main__":
@@ -129,7 +130,7 @@ if __name__ == "__main__":
 
     # Take inputs from the command line
     # Path to the directory containing the data files
-    data_basepath = sys.argv[1]
+    data_path = sys.argv[1]
     # Path to the directory where the results will be saved
     # save_location = sys.argv[12]
     # Averaging window length in seconds
@@ -161,20 +162,40 @@ if __name__ == "__main__":
     # Get the file names of the data files by going through the folders
     # contained in the base path and putting together the paths to files
     # ending in .h5
-    data_files = []
-    for dir_path, dir_names, file_names in os.walk(data_basepath):
-        dir_names.sort()
-        file_names.sort()
-        data_files.extend(
-            [
-                os.path.join(dir_path, file_name)
-                for file_name in file_names
-                if ".h5" in file_name
-            ]
-        )
+    spool = dc.spool(data_path)
+
+    # chunk the spool into averaging_window length
+    spool = spool.chunk(time=averaging_window_length)
+
+    # subselect n_channels number of channels starting from start_channel
+    sub_spool = spool.select(distance=(first_channel,first_channel+channel_offset), samples=True)
+
+    # another way to subselect channels
+    # sub_patch = patch.select(distance=np.array([0, 12, 10, 9]), samples=True)
+
+    # perform coherence calculation on each patch
+    map_out = sub_spool.map(lambda x: f.coherence(x.data.T, sub_window_length,
+                    overlap,
+                    sample_interval=1 / samples_per_sec,
+                    method=method,))
+    
+    detection_significance = np.stack([a[0] for a in map_out], axis=-1)
+    eig_estimates = np.stack([a[1] for a in map_out], axis=-1)
+
+    # data_files = []
+    # for dir_path, dir_names, file_names in os.walk(data_path):
+    #     dir_names.sort()
+    #     file_names.sort()
+    #     data_files.extend(
+    #         [
+    #             os.path.join(dir_path, file_name)
+    #             for file_name in file_names
+    #             if ".h5" in file_name
+    #         ]
+    #     )
 
     # use all the files if batch size is specified as 0
-    batch_size = len(data_files) if batch_size == 0 else batch_size
+    # batch_size = len(data_files) if batch_size == 0 else batch_size
 
     # create a dictionary to store the metadata of the files
     metadata = {}
@@ -200,22 +221,29 @@ if __name__ == "__main__":
             data_files = data_files[(batch - 1) * batch_size - 1 :]
             metadata["files"] = [a[-15:-3] for a in data_files]
 
-    next_index = 0
-    data, next_index, stop_sample_index = _next_data_window(
-        data_files, next_index, averaging_window_length, samples_per_sec
-    )
+    # next_index = 0
+    # data, next_index, stop_sample_index = _next_data_window(
+    #     data_files, next_index, averaging_window_length, samples_per_sec
+    # )
 
     # work on files after first file in batch. This works exactly as we
     # handled the beginning of later batches. Then we keep appending to
     # the variables set up for first file of the batch above
     if method in METHODS:
-        detection_significances, eig_estimatess = func.coherence(
-            data,
-            sub_window_length,
-            overlap,
-            sample_interval=1 / samples_per_sec,
-            method=method,
-        )
+        # detection_significances, eig_estimatess = func.coherence(
+        #     data,
+        #     sub_window_length,
+        #     overlap,
+        #     sample_interval=1 / samples_per_sec,
+        #     method=method,
+        # )
+        map_out = sub_spool.map(lambda x: f.coherence(x.data.T, sub_window_length,
+                    overlap,
+                    sample_interval=1 / samples_per_sec,
+                    method=method,))
+    
+        detection_significance = np.stack([a[0] for a in map_out], axis=-1)
+        eig_estimates = np.stack([a[1] for a in map_out], axis=-1)
     else:
         error_msg = f"Method {method} not available for coherence analysis"
         raise ValueError(error_msg)
@@ -224,51 +252,51 @@ if __name__ == "__main__":
     print(f"First file completed in: {end_time - start_time}", flush=True)
 
     # for a in data_files[1:]:
-    while next_index < len(data_files) - 1:
-        data, next_index, stop_sample_index = _next_data_window(
-            data_files,
-            next_index,
-            averaging_window_length,
-            samples_per_sec,
-            stop_sample_index,
-        )
+    # while next_index < len(data_files) - 1:
+    #     data, next_index, stop_sample_index = _next_data_window(
+    #         data_files,
+    #         next_index,
+    #         averaging_window_length,
+    #         samples_per_sec,
+    #         stop_sample_index,
+    #     )
 
-        if data.shape[1] == averaging_window_length * samples_per_sec:
-            detection_significance, eig_estimates = func.coherence(
-                data,
-                sub_window_length,
-                overlap,
-                sample_interval=1 / samples_per_sec,
-                method=method,
-            )
+    #     if data.shape[1] == averaging_window_length * samples_per_sec:
+    #         detection_significance, eig_estimates = func.coherence(
+    #             data,
+    #             sub_window_length,
+    #             overlap,
+    #             sample_interval=1 / samples_per_sec,
+    #             method=method,
+    #         )
 
-            if detection_significance.shape == detection_significances.shape:
-                detection_significances = np.append(
-                    detection_significances[np.newaxis],
-                    detection_significance[np.newaxis],
-                    axis=0,
-                )
-            else:
-                detection_significances = np.append(
-                    detection_significances,
-                    detection_significance[np.newaxis],
-                    axis=0,
-                )
+    #         if detection_significance.shape == detection_significances.shape:
+    #             detection_significances = np.append(
+    #                 detection_significances[np.newaxis],
+    #                 detection_significance[np.newaxis],
+    #                 axis=0,
+    #             )
+    #         else:
+    #             detection_significances = np.append(
+    #                 detection_significances,
+    #                 detection_significance[np.newaxis],
+    #                 axis=0,
+    #             )
 
-            eig_estimatess = np.append(eig_estimatess, eig_estimates, axis=1)
-        else:
-            print(
-                f"Data length of {data.shape[1]} not the expected"
-                " {averaging_window_length * samples_per_sec} for analysis."
-                " {len(data_files) - next_index} files still remaining ",
-                flush=True,
-            )
+    #         eig_estimatess = np.append(eig_estimatess, eig_estimates, axis=1)
+    #     else:
+    #         print(
+    #             f"Data length of {data.shape[1]} not the expected"
+    #             " {averaging_window_length * samples_per_sec} for analysis."
+    #             " {len(data_files) - next_index} files still remaining ",
+    #             flush=True,
+    #         )
 
-    print(
-        f"Finished in: {datetime.now()-start_time} for {method} method."
-        " Saving to file...",
-        flush=True,
-    )
+    # print(
+    #     f"Finished in: {datetime.now()-start_time} for {method} method."
+    #     " Saving to file...",
+    #     flush=True,
+    # )
 
     # save the results of detection significance, eigenvalues, and metadata to
     # different files
@@ -277,14 +305,14 @@ if __name__ == "__main__":
         f"{method}_detection_significance_{metadata['files'][0]}_{metadata['files'][-1]}.pkl",
     )
     with open(savename, "wb") as f:
-        pickle.dump(detection_significances, f)
+        pickle.dump(detection_significance, f)
 
     savename = os.path.join(
         save_location,
         f"{method}_eig_estimatess_{metadata['files'][0]}_{metadata['files'][-1]}.pkl",
     )
     with open(savename, "wb") as f:
-        pickle.dump(eig_estimatess, f)
+        pickle.dump(eig_estimates, f)
 
     savename = os.path.join(
         save_location,
